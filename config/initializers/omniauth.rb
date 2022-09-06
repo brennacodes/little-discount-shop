ec781d2b0245d09bad4e0c7a09c3d351cc55b632
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-05 21:21:43 -0600

    Update omniauth.rb; clean up old code

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,10 +2,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
 OmniAuth.config.allowed_request_methods = %i[get]
-
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :google_oauth2, 
-#   Rails.application.credentials[:google_authentication][:client_id], 
-#   Rails.application.credentials[:google_authentication][:client_secret], 
-#   skip_jwt: true
-# end
