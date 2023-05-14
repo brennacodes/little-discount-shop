f3ba0fec32b002778c6a1ee2acbb835c2353f4e3
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2023-05-13 19:58:40 -0600

    Uncomment seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@ User.destroy_all
 
 User.create!(username: "admin", password: "password", password_confirmation: "password", email: "brennacodes+discountshop@gmail.com", role: "admin")
 10.times { FactoryBot.create(:user) }
-# 20.times { FactoryBot.create(:merchant) }
-# 50.times { FactoryBot.create(:customer) }
-# 60.times { FactoryBot.create(:invoice) }
-# 70.times { FactoryBot.create(:item) }
-# 70.times { FactoryBot.create(:invoice_item) }
-# 10.times { FactoryBot.create(:discount) }
-# 100.times { FactoryBot.create(:discount_invoice_item) }
+20.times { FactoryBot.create(:merchant) }
+50.times { FactoryBot.create(:customer) }
+60.times { FactoryBot.create(:invoice) }
+70.times { FactoryBot.create(:item) }
+70.times { FactoryBot.create(:invoice_item) }
+10.times { FactoryBot.create(:discount) }
+100.times { FactoryBot.create(:discount_invoice_item) }
