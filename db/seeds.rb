c025bb04e0ba8023b94c22380bf8738a7ea54378
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2023-05-13 18:00:00 -0600

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 User.create!(username: "admin", password: "password", password_confirmation: "password",email: "brennacodes+discountshop@gmail.com", role: "admin")
 20.times { FactoryBot.create(:merchant) }
-40.times { FactoryBot.create(:item) }
-50.times { FactoryBot.create(:invoice_item) }
+50.times { FactoryBot.create(:customer) }
+60.times { FactoryBot.create(:invoice) }
+70.times { FactoryBot.create(:item) }
+70.times { FactoryBot.create(:invoice_item) }
 10.times { FactoryBot.create(:discount) }
 100.times { FactoryBot.create(:discount_invoice_item) }
